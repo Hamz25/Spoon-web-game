10530db11cfb8aecdcae8f140110b59d22817769
Hamz25 <h1a2m3z4ahamza@gmail.com>
2026-09-06 20:12:37 +0300

    creating the Loop and Vercrtor Logic with the input manager

---
+++ src/engine/input/InputManager.js
@@ -0,0 +1,17 @@
+// THis class will handle any input from the user and based on that input will update the game state
+
+class InputManager {
+    constructor() { // The constructor will initialize the input manager and set up event listeners for key presses
+        this.keys = new Set();
+        window.addEventListener('keydown', e => this.keys.add(e.code));
+        window.addEventListener('keyup', e => this.keys.delete(e.code));
+    }
+    // this is a function that will check the input value of the key and return true if the key is pressed and false if it is not pressed
+    isKeyPressed(keyCode) {
+        const map = { left: ['ArrowLeft', 'KeyA'], // Left arrow key or A key
+                    right: ['ArrowRight', 'KeyD'], // Right arrow key or D key
+                    jump: ['ArrowUp', 'KeySpace'], // Up arrow key or W key
+                    crouch: ['ArrowDown', 'KeyS'] }; // Down arrow key or S key
+        return map[keyCode].some(code => this.keys.has(code)); // Return the mapped key code if it is pressed
+    }
+}
